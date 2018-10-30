3dbb4a6d432dd15f89cb01b4dd15241714bc1be3
casa <cuijiemmx@163.com>
2018-10-30 13:53:08 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const fetch = function(url, { method, body, headers }) {
+  return new Promise(resolve => {
+    wx.request({
+      url,
+      method,
+      header: headers,
+      data: body,
+      dataType: 'text',
+      complete: ({ statusCode, data, errMsg, header }) => {
+        resolve({
+          ok: statusCode >= 200 && statusCode < 300,
+          status: statusCode,
+          statusText: errMsg,
+          headers: header,
+          text: () => Promise.resolve(data)
+        })
+      }
+    })
+  })
+}
+
+export default fetch
